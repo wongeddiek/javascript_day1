6d22ad49167368a01715a606f7573479a555ad1d
Edward Wong <wong.eddiek@gmail.com>
2017-07-24 09:18:50 -0600

    Removed primefinder.js to its own project

--- scripts/min/primenumfinder-min.js
+++ scripts/min/primenumfinder-min.js
@@ -21,7 +21,7 @@ function findPrime(max) {
 // var array = [2,3,4,5,6,7,8,9,10]
 
 // Prime finder using Sieve of Eratosthenes algorithm
-function findPrime(max) {
+function findPrimes(max) {
   var arr = []
   for (var i = 2; i <= max; i++) {
     arr.push(i)
